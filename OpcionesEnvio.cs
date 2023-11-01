b7c50d958fec49cfeeaf3cf7f46d362331ed9766
danielmlealc <danielm.lealc@konradlorenz.edu.co>
2023-10-31 20:28:05 -0500

    initial commit

---
+++ OpcionesEnvio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tallerTerceraLetra
+{
+    internal interface OpcionesEnvio
+    {
+        public void SentEmail()
+        {
+            Console.WriteLine("Enviando por email...");
+        }
+
+        public void SentFax()
+        {
+            Console.WriteLine("Enviando por fax...");
+        }
+    }
+}
---
+++ OpcionesLectura.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tallerTerceraLetra
+{
+    internal interface OpcionesLectura
+    {
+        public void Print() //
+        {
+            Console.WriteLine("Imrpimiendo...");
+        }
+        public void Photocopy() //
+        {
+            Console.WriteLine("Copiando...");
+        }
+        public void Cancel() //
+        {
+            Console.WriteLine("Cancelado.");
+        }
+    }
+}
