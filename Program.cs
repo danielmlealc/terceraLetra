732377dc256c1bfa5f7634f225da1c31a59cf478
David Chaves Gutierrez <142677288+DAID04@users.noreply.github.com>
2023-11-07 19:25:29 -0500

    Update Program.cs
    Cambios del program para funcionamiento, solo falta implementar las clases

--- Program.cs
+++ Program.cs
@@ -1,10 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 ﻿namespace tallerTerceraLetra
 {
-    internal class Program
+    public class PrinterModel2000 : IMultifunctionalPrinter
     {
-        static void Main(string[] args)
+        public void Cancel() //
         {
-            Console.WriteLine("Hello, World!");
+            throw new NotImplementedException();
+        }
+
+        public void Photocopy() //
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Print() //
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SentEmail()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SentFax()
+        {
+            throw new NotImplementedException();
         }
     }
+
+
 }
